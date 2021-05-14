acb399aaf8b9a3ce60290ed77ae60f48e98c7d8c
Manuel G <manuel.geissberger@gmail.com>
2021-05-14 13:08:23 +0200

    [ADD] better css styling, dark-theme and light-theme

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+function toggleDarkTheme(event) {
+    if (event.target.value === 'light') {
+        document.body.classList.remove('dark-theme');
+    } else {
+        document.body.classList.add('dark-theme');
+    }
+}
+
+const selectTheme = document.querySelector('[data_theme]');
+selectTheme.addEventListener('change', (event) => toggleDarkTheme(event));
