ade14b9d1f7f99622c2673e85a4ef8bcc577c5bb
Manuel G <manuel.geissberger@gmail.com>
2021-05-15 12:27:12 +0200

    [ADD] basic Dom manipulation
    Creating a new Task-card with element[0] form the ToDoManager.tasklist

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -13,6 +13,11 @@ const selectTheme = document.querySelector('[data_theme]');
 selectTheme.addEventListener('change', (event) => toggleDarkTheme(event));
 
 const toDoManager = new ToDoManager();
-const task = new Task('my Title', 'my Description which is very long', 1);
+const task = new Task('Visit Grandma', 'Visit grandma. Don\'t forget to be hungry! There will be lots of cake!', 1, new Date(2020, 7, 5));
 toDoManager.addTask(task);
-console.log(toDoManager.toString());
+
+const taskList = document.querySelector('[data-task-list]');
+const newCard = document.createElement('div');
+newCard.classList.add('task-card');
+newCard.innerHTML = toDoManager.taskList[0].getHtmlText();
+taskList.appendChild(newCard);
--- public/scripts/models/task.js
+++ public/scripts/models/task.js
@@ -1,8 +1,9 @@
 export default class Task {
-    constructor(title, description, importance) {
+    constructor(title, description, importance, dueDate) {
         this.title = title;
         this.description = description;
         this.finishDate = new Date();
+        this.dueDate = dueDate;
         this.createDate = new Date();
         this.importance = importance;
         this.finised = false;
@@ -12,9 +13,22 @@ export default class Task {
         return `[${this.importance}][${this.title}]: "${this.description}", start: ${this.createDate} - ${this.finishDate} finished: ${this.finished}`;
     }
 
-    getHtmlText(){
-        let output =`
-            
+    getHtmlText() {
+        return `
+        <h1>${this.title}</h1>
+        <div class="display-date">
+          <p class='label'>due:</p>
+          <p class='date'>${this.dueDate.getDay()}/${this.dueDate.getMonth()}/${this.dueDate.getFullYear()}</p>
+        </div>
+        <div class="display-date line-bottem">
+          <p class='label'>finished:</p>
+          <p class='date'>${this.finishDate.getDay()}/${this.finishDate.getMonth()}/${this.finishDate.getFullYear()}</p>
+        </div>
+        <p class="description">${this.description}</p>
+        <div class="button-holder">
+          <button class="btn positive">Done</button>
+          <button class="btn negative">Cancel</button>
+        </div>        
         `;
     }
 }
