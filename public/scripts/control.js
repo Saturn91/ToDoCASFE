35cf54b815c7b3e8c8c998c2717c664494d80818
Manuel G <manuel.geissberger@gmail.com>
2021-05-17 14:57:51 +0200

    [ADD] warning msg on submit and not valid

--- public/scripts/control.js
+++ public/scripts/control.js
@@ -1,10 +1,19 @@
 const closeBtns = document.querySelectorAll('[data-close-popup]');
 const popUpWindow = document.querySelector('[data-popup]');
 const addTaskBtn = document.querySelector('[data-add-task]');
+const newTaskSubmitBtn = document.querySelector('[data-submit-task]');
+const warningDisplay = document.querySelector('[data-form-msg]');
+
+/* Popup show/hide */
 
 function showPopup(show) {
     if (show) {
         popUpWindow.style.display = 'block';
+        warningDisplay.style.display = 'none';
+        /* Set Date to default 'today' */
+        const local = new Date();
+        local.setMinutes(local.getMinutes() - local.getTimezoneOffset());
+        popUpWindow.querySelector('input[name="duedate"]').value = local.toJSON().slice(0, 10);
     } else {
         popUpWindow.style.display = 'none';
     }
@@ -20,4 +29,35 @@ closeBtns.forEach((item) => {
     item.addEventListener('click', () => {
         showPopup(false);
     });
+});
+
+/* submit new task */
+
+function showInvalid(msg) {
+    warningDisplay.style.display = 'block';
+    warningDisplay.textContent = msg;
+}
+
+function checknewTaskFormValid() {
+    let valid = true;
+    if (!popUpWindow.querySelector('input[name="description"]').checkValidity()) {
+        showInvalid('please enter a description');
+        valid = false;
+    }
+    if (!popUpWindow.querySelector('input[name="prio"]').checkValidity()) {
+        showInvalid('priority has to be a value between 1 and 3');
+        valid = false;
+    }
+    if (!popUpWindow.querySelector('input[name="title"]').checkValidity()) {
+        showInvalid('please enter a title');
+        valid = false;
+    }
+    return valid;
+}
+
+newTaskSubmitBtn.addEventListener('click', () => {
+   if (checknewTaskFormValid()) {
+       warningDisplay.style.display = 'none';
+       showPopup(false);
+   }
 });
